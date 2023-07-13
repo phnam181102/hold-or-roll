0202bf1ccf701bb4f3830ef92a3191e54857899d
Nam Phan <phnam181102@gmail.com>
2023-07-13 21:08:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Game Instructions
+
+1. **Objective:**
+   The objective of the game is to be the first player to reach a total of 100 points.
+
+2. **Gameplay:**
+   - Two players take turns rolling a six-sided die.
+   - On your turn, roll the die and accumulate points based on the number rolled.
+   - If you roll a number from 2 to 6, you have two options:
+     - **Roll Again:** Choose to roll the die again to add the newly rolled points to your current turn's total.
+     - **Hold:** Decide to hold and add your current turn's points to your overall score. It will then be the other player's turn.
+   - If you roll a 1, your current turn's points are lost, and it becomes the other player's turn immediately.
+
+3. **Strategic Choices:**
+   - The decision to hold or roll again is crucial. You need to balance the risk and reward.
+   - Rolling again gives you a chance to accumulate more points but also carries the risk of rolling a 1 and losing all your turn's points.
+   - Holding ensures that your current turn's points are added to your total score but leaves the remaining points for your opponent to potentially grab.
+
+4. **Winning the Game:**
+   The player who reaches a total of 100 points first is declared the winner. Keep a close eye on your opponent's score and strategize accordingly.
+
+Remember, luck and decision-making skills are key in the game! Are you ready to take on the challenge and emerge victorious?
+
+![Game Preview](./assets/imgs/preview.png)
---
+++ script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const diceEl = document.querySelector('.dice');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const winner0El = document.querySelector('.winner--0');
+const winner1El = document.querySelector('.winner--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0EL = document.querySelector('#current--0');
+const current1EL = document.querySelector('#current--1');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore, currentPlayer, totalScore, playing;
+
+const init = () => {
+    currentScore = 0;
+    currentPlayer = 0;
+    totalScore = [80, 80];
+    playing = true;
+
+    diceEl.classList.add('hidden');
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0EL.textContent = 0;
+    current1EL.textContent = 0;
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    winner0El.classList.add('hidden');
+    winner1El.classList.add('hidden');
+};
+init();
+
+const switchPlayer = () => {
+    currentScore = 0;
+    document.querySelector(`#current--${currentPlayer}`).textContent = 0;
+
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
+btnRoll.addEventListener('click', () => {
+    if (playing) {
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        diceEl.classList.remove('hidden');
+        diceEl.src = `./assets/imgs/dice-${dice}.png`;
+
+        if (dice !== 1) {
+            currentScore += dice;
+            document.querySelector(`#current--${currentPlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
+    }
+});
+
+btnHold.addEventListener('click', () => {
+    if (playing) {
+        totalScore[currentPlayer] += currentScore;
+
+        document.getElementById(`score--${currentPlayer}`).textContent = totalScore[currentPlayer];
+        console.log(totalScore[currentPlayer]);
+        if (totalScore[currentPlayer] >= 100) {
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.winner--${currentPlayer}`).classList.remove('hidden');
+            document.querySelector(`.player--${currentPlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${currentPlayer}`).classList.remove('player--active');
+        } else {
+            switchPlayer();
+        }
+    }
+});
+
+btnNew.addEventListener('click', init);
